c9d857b1fdc79e80b66b3ccc9421aba1ab9d844e
NechipurenkoDmitriy <pikinaplecho@gmail.com>
2022-11-11 00:20:03 +0300

    hw_rrouter6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Navigate, NavLink, Route, Routes, useParams} from 'react-router-dom';
+
+const usersIds = [1, 2, 3, 5, 20];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route index element={<HomePage />} />
+
+				<Route path="users">
+					<Route index element={<UsersListPage />} />
+
+					<Route path=":userId" >
+						<Route index element={<UserPage />} />
+						<Route path="edit" element={<UserEditPage />} />
+						<Route path="*"	element={<Navigate to={''} />} />
+					</Route>
+				</Route>
+
+				<Route path="*" element={<Navigate to={'/'} />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
+
+const HomePage = () => {
+	return (
+		<>
+			<h1>Home Page</h1>
+			<NavLink to={'/users'}>Users List</NavLink>
+		</>
+	);
+};
+
+const UsersListPage = () => {
+	return (
+		<>
+			<h1>UsersList Page</h1>
+
+			<NavLink to={'/'}>Home</NavLink>
+			<hr />
+			{usersIds.map((id) => {
+				return (
+					<ul key={'ul'+id}>
+						<NavLink key={id} to={`/users/${id}`}>
+							User Page {id}
+						</NavLink>
+					</ul>
+				);
+			})}
+		</>
+	);
+};
+
+const UserPage = () => {
+	const {userId} = useParams()
+	return (
+		<>
+			<h1>User Page</h1>
+			<NavLink to={`/users/${userId}/edit`}>Edit user {userId}</NavLink>
+			{' '}
+			<NavLink to={`/users`}>Users list</NavLink>
+		</>
+	);
+};
+
+const UserEditPage = () => {
+	const {userId} = useParams()
+	const filteredUsersIds = usersIds.filter((id)=>String(id) !== userId)
+	
+	return (
+		<>
+			<h1>User Info Page</h1>
+			<NavLink to={`/users/${userId}`}>Back to user {userId}</NavLink>
+			{' '}
+			<NavLink to={`/users`}>Users list</NavLink>
+			<hr/>
+			{filteredUsersIds.map((id) => {
+				return (
+					<ul key={'ul'+id}>
+						<NavLink key={id} to={`/users/${id}`}>
+							User Page {id}
+						</NavLink>
+					</ul>
+				);
+			})}
+
+		</>
+	);
+};
+
